a4f6e72c504b5711d07946399f72dff7d5365304
Simon Christmann <simon@christmann.email>
2021-03-11 15:29:43 +0100

    rm unnecessary stuff

--- stories/Button.js
+++ stories/Button.js
@@ -1,12 +1,9 @@
 import { html } from 'lit-html';
-import './button.css';
 
 /**
  * Primary UI component for user interaction
  */
 export const Button = ({ primary, backgroundColor, size, label }) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
-
   function internalOnClickFunction(e) {
     console.log('logged from internalOnClickFunction', e);
 
@@ -18,8 +15,6 @@ export const Button = ({ primary, backgroundColor, size, label }) => {
     <button
       id="mySelector"
       type="button"
-      class=${['storybook-button', `storybook-button--${size || 'medium'}`, mode].join(' ')}
-      style=${backgroundColor && { backgroundColor }}
       @click=${internalOnClickFunction}
     >
       ${label}
